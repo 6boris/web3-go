a9cf0c3bc6f8d04890a8dda222a0674e6dbb33c2
Boris <boris.liu1@gmail.com>
2023-06-21 15:17:04 +0800

    feat: update otel

--- pkg/otel/metrics.go
+++ pkg/otel/metrics.go
@@ -1,25 +1,20 @@
 package otel
 
 import (
-	"go.opentelemetry.io/otel/exporters/prometheus"
+	otelProm "go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/global"
 	metricSdk "go.opentelemetry.io/otel/sdk/metric"
 )
 
 var MetricsWeb3RequestCounter metric.Int64Counter
 var MetricsWeb3RequestHistogram metric.Int64Histogram
 
-func InitProvider() {
-	exporter, err := prometheus.New()
+func init() {
+	exporter, err := otelProm.New()
 	if err != nil {
 		panic(err)
 	}
 	provider := metricSdk.NewMeterProvider(metricSdk.WithReader(exporter))
-	global.SetMeterProvider(provider)
-}
-func init() {
-	provider := global.MeterProvider()
 	meter := provider.Meter("Web3 Go")
 	m1, err := meter.Int64Counter("web3_abi_call", metric.WithDescription("Web3 Gateway abi call counter"))
 	if err != nil {
